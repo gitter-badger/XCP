ea39459797c220082d1726b6053b1f19abf3dfd5
Benjamin Garside <abgarside@gmail.com>
2015-09-03 09:17:40 +0100

    fixes #47

--- flow.php
+++ flow.php
@@ -26,8 +26,8 @@ if($stage = Input::get('stage')){
 	<form action="" method="post" class="form-horizontal">
 
 	  <div class="form-group" id="name_form">
-	    <label for="name" class="col-sm-2 control-label">Name</label>
-	    <div class="col-sm-10">
+	    <label for="name" class="col-sm-3 control-label">Name</label>
+	    <div class="col-sm-6">
 	     	<input value="<?php echo $statusInfo->name ;?>" type="text" class="form-control" name="terst" id="name">
 	     	<span id="feedbackSuccess_name" style="display: none" class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
 	      	<span id="inputSuccess3Status_name" style="display: none" class="sr-only">(success)</span>
@@ -36,9 +36,9 @@ if($stage = Input::get('stage')){
 	    </div>
 	  </div>
 	  <div class="form-group" id="desc_form">
-	    <label for="description" class="col-sm-2 control-label">Description</label>
-	    <div class="col-sm-10">
-	     	 <input value="<?php echo $statusInfo->description ;?>" type="text" class="form-control" id="description">
+	    <label for="description" class="col-sm-3 control-label">Description</label>
+	    <div class="col-sm-6">
+	     	 <textarea class="form-control" id="description"><?php echo $statusInfo->description ;?></textarea>
 	      	<input value="<?php echo $statusInfo->id ;?>" type="hidden" id="statusId">
 			<span id="feedbackSuccess_desc" style="display: none" class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
 			<span id="inputSuccess3Status_desc" style="display: none" class="sr-only">(success)</span>
@@ -67,8 +67,8 @@ if($stage = Input::get('stage')){
 			    		<div class="editRule">
 							<div class="form-group" id="<?php echo $rule['id'] ;?>">
 							  <p class="col-sm-1" style="padding-top: 7px;"><span class="label label-default"><?php echo $rule['id'] ;?></span></p>
-							  <label class="control-label col-sm-1 " for="activity_<?php echo $rule['id'] ;?>">Destination</label>
-							  <div class="col-sm-1">
+							  <label class="control-label col-sm-2 " for="activity_<?php echo $rule['id'] ;?>">Destination</label>
+							  <div class="col-sm-2">
 							  <select  class="form-control activity" id="activity_<?php echo $rule['id'] ;?>" aria-describedby="inputSuccess2Status">
 								<option  value="" disabled >Activity</option>
 								<?php
@@ -85,7 +85,7 @@ if($stage = Input::get('stage')){
 								  ?>
 							  </select>
 							  </div>			  
-							  <div class="col-sm-1">
+							  <div class="col-sm-2">
 							  <select type="text" class="form-control status" id="status_<?php echo $rule['id'] ;?>" aria-describedby="inputSuccess2Status">
 								  <option class="status" value="" disabled >Status</option>
 									<?php
@@ -108,8 +108,8 @@ if($stage = Input::get('stage')){
 							  </div>
 							</div>
 							<div class="form-group">
-							  <label class="control-label col-sm-2 " for="action_<?php echo $rule['id'] ;?>">Action</label>
-							  <div class="col-sm-3">
+							  <label class="control-label col-sm-3 " for="action_<?php echo $rule['id'] ;?>">Action</label>
+							  <div class="col-sm-4">
 							  <select class="form-control action" id="action_<?php echo $rule['id'] ;?>" aria-describedby="inputSuccess2Status">
 							  	<option value="" selected >None</option>
 									<?php
@@ -127,7 +127,7 @@ if($stage = Input::get('stage')){
 							  </div>
 							</div>
 							<div class="form-group">
-								<div class="col-sm-offset-2 col-sm-10">
+								<div class="col-sm-offset-3 col-sm-10">
 									<div class="checkbox">
 										<label>
 										<input id="assign_<?php echo $rule['id'] ;?>" type="checkbox" value="" <?php if($rule['assign'] == 1){ echo " checked";} ;?>> Sticky assignment
@@ -136,7 +136,7 @@ if($stage = Input::get('stage')){
 								</div>
 							</div>
 							<div class="form-group">
-							  <label class="control-label col-sm-2 " for="inputSuccess2"></label>
+							  <label class="control-label col-sm-3 " for="inputSuccess2"></label>
 							  <div class="col-sm-4">
 							  	<button type="button" onclick="removeRule('editRule_<?php echo $rule['id'] ;?>')" class="btn btn-danger"><i class="fa fa-trash-o"></i> Delete</button>
 								<!--<button type="button" onclick="addRuleInput(<?php echo "'" . $ruleset . "'"; ?>)" class="btn btn-warning"><i class="fa fw fa-plus"></i> Add rule</button>-->
